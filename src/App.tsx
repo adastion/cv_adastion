7b97a93cd6d6120401b532b3997f5f3105696a15
Andrey Sery <andrey.72006@gmail.com>
2024-11-05 23:46:11 +0300

    creating new cv project + ts

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <header>
+        <div className="logo"></div>
+        <nav className="navigation">
+          <ul className="navigation__list">
+            <li className="navigation__item"></li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
